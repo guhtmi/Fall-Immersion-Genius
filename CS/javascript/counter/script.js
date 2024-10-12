7348142f42275ceadd7ec6958e268bc6a7bfd997
Aiden Timms * <guhtmi@TheHiddProject2.lan>
2024-10-12 11:55:07 -0700

    saturday push #pregnancy

--- CS/javascript/counter/script.js
+++
@@ -1,4 +0,0 @@
-function counter() {
-   counter_number = document.getElementById('counter-number');
-   counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1; 
-    }
--- CS/javascript/event-handling/script.js
+++
@@ -1,3 +0,0 @@
-function eventHandling() {
-    // Your code here.
-}
